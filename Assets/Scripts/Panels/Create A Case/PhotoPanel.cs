8d5f6814bf0d0c9f50d8f976859b7a50e1d45fd0
NovusX-Dev <novusxdev@outlook.com>
2021-07-16 06:35:07 -0300

    Take Photo complete

--- Assets/Scripts/Panels/Create A Case/PhotoPanel.cs
+++ Assets/Scripts/Panels/Create A Case/PhotoPanel.cs
@@ -21,5 +21,39 @@ public class PhotoPanel : MonoBehaviour, IPanel
         {
             UIManager.Instance.activeCase.photoNotes = _photoNotes.text;
         }
+
+        if (_rawPhotoTaken.texture != null)
+        {
+            UIManager.Instance.activeCase.photoTaken.texture = _rawPhotoTaken.texture;
+        }
+    }
+
+    public void TakePictureButton()
+    {
+        TakePicture(512);
+    }
+
+    private void TakePicture(int maxSize)
+    {
+        NativeCamera.Permission permission = NativeCamera.TakePicture( ( path ) =>
+        {
+            Debug.Log( "Image path: " + path );
+            if( path != null )
+            {
+                // Create a Texture2D from the captured image
+                Texture2D texture = NativeCamera.LoadImageAtPath( path, maxSize );
+                if( texture == null )
+                {
+                    Debug.Log( "Couldn't load texture from " + path );
+                    return;
+                }
+
+                _rawPhotoTaken.texture = texture;
+                _rawPhotoTaken.gameObject.SetActive(true);
+                //_takePhoto.gameObject.SetActive(false);
+            }
+        }, maxSize );
+
+        Debug.Log( "Permission result: " + permission );
     }
 }
